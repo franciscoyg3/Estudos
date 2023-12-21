f6627a6fb1ae57157a4d78a0cd3c707c6e129f0d
francisoyg3 <franciscoyg3@users.noreply.github.com>
2023-12-21 17:14:29 +0100

    Initial commit

---
+++ Desafios/desafio 6/stdio.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+
+int main{
+    int i;
+
+    for(i=0;i<4;i++){
+        printf("Porra combinamos tats, quase escrevi o teu nome\n");
+    }
+    system("pause");
+    return 0;
+}
